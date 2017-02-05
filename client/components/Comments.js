b8f988a237893d58fe25d58791900b9a778daeff
Daniel Deutsch <deudan1010@gmail.com>
2017-02-05 21:27:47 +0100

    13 displaying single comment

---
+++ client/components/Comments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render() {
+    return (
+      <div className="comment">
+        Iam a comment
+      </div>
+    );
+  }
+});
+
+export default Comments;
