169b490957b959cf828b79b6efe780bccbd0d708
Daniel Deutsch <deudan1010@gmail.com>
2017-02-06 11:16:45 +0100

    17 error tracking

--- client/data/config.js
+++ client/data/config.js
@@ -1,7 +1,7 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
+const sentry_key = 'b68bac174aad4d6599cd681e3a47e009';
+const sentry_app = '135929';
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
 export function logException(ex, context) {
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -14,6 +14,25 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
+//import sentry
+import Raven from 'raven-js';
+import {sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url, {
+  tags: {
+    git_commit: 'git tag',
+    userLevel: 'editor'
+  }
+}).install();
+
+// logException(new Error('download failed!'), {
+//   email: 'danimail'
+// })
+Raven.captureMessage('Something went to shit');
+Raven.showReportDialog();
+
+console.log(window.adsfasdf);
+
 const router = (
   <Provider store={store}>
     <Router history={history}>
